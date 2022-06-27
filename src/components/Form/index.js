4c574cb4408efda00dcbd9f653445de03b38a763
Tiago Rodrigues <tiagorodriguessousa9@gmail.com>
2022-06-26 21:57:17 -0300

    Added logic to calculate BMI

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -5,15 +5,48 @@ import OperationResult from '../Form/OperationResult';
 export default function Form() {
     const [message, setMessage] = React.useState(null);
     const [result, setResult] = React.useState(null);
+    const [weight, setWeight] = React.useState(null);
+    const [height, setHeight] = React.useState(null);
+    const [textButton, setTextButton] = React.useState("Calculate BMI");
+
+    function calculateBMI() {
+        return setResult((weight/(height*height)).toFixed(2));
+    }
+
+    function validationBMI() {
+        if(weight != null && height != null) {
+            calculateBMI();
+            setWeight(null);
+            setHeight(null);
+            setMessage("Your BMI is: ");
+            setTextButton("Calculate again");
+        } else {
+            setResult(null)
+            setTextButton("Calculate");
+            setMessage("Fill fields with your informations");
+        }
+    }
 
     return (
         <View>
             <View>
                 <Text>Weight</Text>
-                <TextInput />
+                <TextInput 
+                placeholder="Ex.: 75.0"
+                onChangeText={setWeight}
+                value={weight}
+                keyboardType='numeric'
+                />
                 <Text>Height</Text>
-                <TextInput/>
-                <TouchableOpacity>Calculate</TouchableOpacity>
+                <TextInput 
+                placeholder="Ex.: 1.75"
+                onChangeText={setHeight}
+                value={height}
+                keyboardType='numeric'
+                />
+                <TouchableOpacity onPress={() => { validationBMI(); }} value={textButton}>
+                    Calculate
+                </TouchableOpacity>
             </View>
             <OperationResult bmiResultMessage={message} bmiResult={result}/>
         </View>
