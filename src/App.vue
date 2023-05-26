bcc1c9484bb085beaef2364e6aef5b24f820de7c
Tiago Rodrigues Sousa <38366602+tiagors09@users.noreply.github.com>
2023-05-26 09:51:52 -0300

    added all code to bmi calc

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,81 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { computed, ref } from 'vue'
+
+const height = ref<number>(0)
+const weight = ref<number>(0)
+
+const bmi = computed<string>(() => {
+  return height.value != 0 && weight.value != 0
+    ? `${(weight.value / Math.pow(height.value, 2)).toFixed(2)} kg/m²`
+    : 'Type your height and weight'
+})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <div class="content">
+      <h1>BMI Calculator</h1>
+      <section>
+        <article>
+          <p>
+            BMI is the acronym for Body Mass Index, a parameter adopted by the World Health Organization to calculate the
+            ideal weight of each person.
+          </p>
+
+          <p>
+            The index is calculated as follows: the patient's weight is divided by their height squared. An individual is
+            said to have normal weight when the BMI result is between 18.5 and 24.9.
+          </p>
+
+          <p>
+            Want to find out your BMI? Enter your weight and height in the fields below and compare with the indexes in
+            the
+            table. Important: follow the examples and use dots as separators.
+          </p>
+        </article>
+      </section>
+
+      <form>
+        <label for="height">Height</label>
+        <input type="number" name="height" id="height" v-model="height">
+
+        <label for="weight">Weight</label>
+        <input type="number" name="weight" id="weight" v-model="weight">
+      </form>
+
+      <span>{{ bmi }}</span>
+    </div>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 20vw;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+p {
+  text-align: justify;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+form {
+  display: flex;
+  flex-direction: column;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+label {
+  margin: 0 0 .25em 0;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+input[type="number"] {
+  margin: 0 0 1em 0;
+}
+
+span {
+  display: flex;
+  align-self: center;
 }
 </style>
